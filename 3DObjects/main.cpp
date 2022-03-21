fd25a3d0b947a0681946a1b364c10a5dbdf97b8e
Ilya-Coder1 <ilya.aleksandrov.2003@gmail.com>
2022-03-21 19:25:26 +0400

    models was added

--- 3DObjects/main.cpp
+++ 3DObjects/main.cpp
@@ -13,17 +13,7 @@ int main()
 	Graphics::Set_pen(RGB(255, 0, 0), 2);
 	Graphics::Set_brush(RGB(0, 0, 0), false);
 
-	Object3D lego_man("LEGO_Man.obj");
-	lego_man.rotate(30, 30, 0).scale(80).move(200, 200);
-
-	//Object3D skull("12140_Skull_v3_L2.obj");
-	//skull.rotate(100, 180, 180).scale(30).move(1200, 100);
-	//skull.draw();
-
-	while (true)
-	{
-		lego_man.draw().move(20, 0);
-		system("cls");
-	}
+	Object3D lego_man("models/LEGO_Man.obj");
+	lego_man.rotate(30, 30, 0).scale(80).move(200, 200).draw();
 
 }
